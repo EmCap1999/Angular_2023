87d46c86a4de7f6e79128b103683eb9d7bda72af
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2023-09-12 12:06:47 +0200

    First Component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+
+//Output some info about a server
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
