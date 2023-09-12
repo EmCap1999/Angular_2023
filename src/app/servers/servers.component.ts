6893a8083d8a3af0745eafd5b6b46fa02fe436b3
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2023-09-12 14:31:44 +0200

    Selector Component manage

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector:'.app-servers',
   selector: 'app-servers',
   template: `
     <app-server></app-server>
