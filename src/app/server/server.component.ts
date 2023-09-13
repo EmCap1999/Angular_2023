d42d2806e2db95fe59d7602e835bce0269ef7535
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2023-09-13 10:15:15 +0200

    config file and dependencies set up

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
 })
 export class ServerComponent {
 
