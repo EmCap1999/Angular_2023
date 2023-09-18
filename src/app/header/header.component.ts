7d7189f89c1b595b5dc40ec8064e300ff51822ad
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2023-09-18 11:46:12 +0200

    Alternative non collapsable - Responsive NavBar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -7,5 +8,6 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
+    collapsed = true;
 
 }
