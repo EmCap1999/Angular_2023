914053223bfc8eb20bf57133459073494b3da596
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2023-09-25 13:44:29 +0200

    small syntax correction

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, OnInit, Output, ViewChild, ElementRef } from '
 })
 export class CockpitComponent implements OnInit{
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent:string}>();
-  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent:string}>();;
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent:string}>();
   // newServerName = '';
   // newServerContent = '';
   @ViewChild('serverContentInput', {static:true}) serverContentInput: ElementRef;
